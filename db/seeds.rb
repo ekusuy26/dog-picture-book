6f35fcb87b8109373047209f4cef896ed2d600d2
ekusuy26 <y.yuu6221@gmail.com>
2020-04-20 11:29:03 +0900

    create root category

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+1G:牧羊犬・牧畜犬
+2G:使役犬
+3G:テリア
+4G:ダックスフンド
+5G:原始的な犬・スピッツ
+6G:嗅覚ハウンド
+7G:ポインター・セター
+8G:7G以外の鳥猟犬
+9G:愛玩犬
+10G:視覚ハウンド
