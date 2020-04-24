d9f73afcf1ad4b5048b30a51cba4a8b0f06f0953
ekusuy26 <y.yuu6221@gmail.com>
2020-04-24 20:27:40 +0900

    complete

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -2,5 +2,6 @@ class DogsController < ApplicationController
   def index
     @dogs = Dog.all
     @all_ranks = Dog.find(Like.group(:dog_id).order('count(dog_id) desc').limit(10).pluck(:dog_id))
+    @comments = Comment.order(id: :desc).limit(5)
   end
 end
