eedeb60150c079524fd3fa68c6b620a252193755
ekusuy26 <y.yuu6221@gmail.com>
2020-04-25 15:44:43 +0900

    Top page markup completed

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -3,5 +3,6 @@ class DogsController < ApplicationController
     @dogs = Dog.all
     @all_ranks = Dog.find(Like.group(:dog_id).order('count(dog_id) desc').limit(10).pluck(:dog_id))
     @comments = Comment.order(id: :desc).limit(5)
+    @parents = Dog.where(ancestry: nil)
   end
 end
