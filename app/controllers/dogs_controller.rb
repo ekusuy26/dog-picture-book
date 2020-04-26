b78e8eef97cae2343290a73fca1c5a1539fa5bd6
ekusuy26 <y.yuu6221@gmail.com>
2020-04-26 14:59:52 +0900

    Randomly extract top page image

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,6 +1,7 @@
 class DogsController < ApplicationController
+
   def index
-    @dogs = Dog.all
+    @dogs = Dog.where.not(image: nil).sample(10)
     @all_ranks = Dog.find(Like.group(:dog_id).order('count(dog_id) desc').limit(10).pluck(:dog_id))
     @comments = Comment.order(id: :desc).limit(5)
     @parents = Dog.where(ancestry: nil)
