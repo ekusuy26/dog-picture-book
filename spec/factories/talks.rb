dbc1c114916ede14a8cce99540f5b72632361cd9
ekusuy26 <y.yuu6221@gmail.com>
2020-04-29 17:28:54 +0900

    Talk model unit test completed

--- spec/factories/talks.rb
+++ spec/factories/talks.rb
@@ -4,6 +4,8 @@ FactoryBot.define do
     text       {"テスト"}
     user_id    {"1"}
     comment_id {"1"}
+    association :user
+    association :comment
   end
 
 end
--- spec/models/talk_spec.rb
+++ spec/models/talk_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 describe Talk do
   describe '#create' do
 
-    # it "textがない場合は登録できないこと" do
-    #   talk = build(:talk, text: "")
-    #   talk.valid?
-    #   expect(talk.errors[:text]).to include("can't be blank")
-    # end
+    it "textがない場合は登録できないこと" do
+      talk = build(:talk, text: "")
+      talk.valid?
+      expect(talk.errors[:text]).to include("can't be blank")
+    end
 
-    # it "textがあれば登録できること " do
-    #   talk = build(:talk, text: "aaaaaa")
-    #   expect(talk).to be_valid
-    # end
+    it "textがあれば登録できること " do
+      talk = build(:talk, text: "aaaaaa")
+      expect(talk).to be_valid
+    end
 
   end
 end
